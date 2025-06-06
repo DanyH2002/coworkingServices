c3eaeef91ce236ced0a39f9590234d5900ad169c
Dany Luna <danielacrisantoluna@gmail.com>
2025-06-05 20:29:20 -0600

    BG 002 corregido

--- app/Http/Controllers/api/UsuariosController.php
+++ app/Http/Controllers/api/UsuariosController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Usuario;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\api\FuncionesController as Email;
 
 class UsuariosController extends Controller
 {
@@ -25,11 +26,20 @@ class UsuariosController extends Controller
         // 0 = usuario, 1 = administrador
         $usuario->rol = 0;
         $usuario->save();
-
+        $token = $usuario->createToken('auth_token')->plainTextToken;
+        $email = $this->getEmailsToNotify($usuario->id);
+        // Enviar correo de bienvenida al usuario
+        Email::sendEmail( //? Enviar correo a los administradores y al cliente
+            $email,
+            'Bienvenido a nuestra plataforma',
+            'Hola ' . $usuario->name . ',Gracias por registrarte en nuestra plataforma. Tu cuenta ha sido creada con éxito.',
+            'Bienvenido'
+        );
         return response()->json([
             'status' => 1,
             'message' => 'Usuario registrado con éxito',
-            'usuario' => $usuario
+            'usuario' => $usuario,
+            'token' => $token,
         ], 201);
     }
 
@@ -75,5 +85,21 @@ class UsuariosController extends Controller
             'status' => 1,
             'message' => 'Usuario deslogueado con éxito',
         ], 200);
+    }
+
+    // Obtener los emails de los usuarios
+    private function getEmailsToNotify($id_user)
+    {
+        $cliente = Usuario::find($id_user); // Obtener el cliente
+        $administradores = Usuario::where('rol', 1)->pluck('email')->toArray(); // Obtener los administradores
+
+        if ($cliente && $cliente->email) { // Verificar si el cliente tiene un email
+            // Agregar el email del cliente a la lista de correos
+            $correos = $administradores; // Iniciar la lista de correos con los administradores
+            $correos[] = $cliente->email; // Agregar el email del cliente
+            // Enviar el correo al cliente
+            return $correos;
+        }
+        return $administradores; // Si no hay cliente, solo devolver los administradores
     }
 }
